7532d8c411d13bece400d6ee0c0168f91c7b62a2
Jim Sellers <jim.sellers@canada.ca>
2020-10-01 18:00:48 -0400

    initial commit

---
+++ README.md
@@ -0,0 +1,5 @@
+# For figuring out an issue when updating owasp
+
+## Setup
+Run the script run-test.sh
+It has a commented out purge of the data directories. Consider running those.
---
+++ run-test.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+# exit if any of the commands return an error
+set -e
+
+OLDVERSION=5.3.2
+NEWVERSION=6.0.2
+
+# purge the repo
+# mvn org.owasp:dependency-check-maven:$OLDVERSION:purge
+# mvn org.owasp:dependency-check-maven:$NEWVERSION:purge
+
+# update the repo
+mvn org.owasp:dependency-check-maven:$OLDVERSION:update-only
+mvn org.owasp:dependency-check-maven:$NEWVERSION:update-only
+
+# run the tests
+mvn -X -B org.owasp:dependency-check-maven:$OLDVERSION:check org.owasp:dependency-check-maven:$OLDVERSION:aggregate \
+  -Dformat=ALL -DskipProvidedScope=true -DautoUpdate=false -DnuspecAnalyzerEnabled=false \
+  -DassemblyAnalyzerEnabled=false -DnodeAuditAnalyzerEnabled=false -DnodeAnalyzerEnabled=false \
+  -DrubygemsAnalyzerEnabled=false -DbundleAuditAnalyzerEnabled=false > $OLDVERSION.log
+  
+mvn -X -B org.owasp:dependency-check-maven:$NEWVERSION:check org.owasp:dependency-check-maven:$NEWVERSION:aggregate \
+  -Dformat=ALL -DskipProvidedScope=true -DautoUpdate=false -DnuspecAnalyzerEnabled=false \
+  -DassemblyAnalyzerEnabled=false -DnodeAuditAnalyzerEnabled=false -DnodeAnalyzerEnabled=false \
+  -DrubygemsAnalyzerEnabled=false -DbundleAuditAnalyzerEnabled=false > $NEWVERSION.log
