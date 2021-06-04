c6c86af1d6269913dd5030dec70fd277dccf1d47
Jim Sellers <jim.sellers@gmail.com>
2021-06-04 13:08:39 -0400

    debugging #3408

--- run-test.sh
+++ run-test.sh
@@ -4,21 +4,21 @@
 set -e
 
 OLDVERSION=6.1.1
-NEWVERSION=6.1.5
+NEWVERSION=6.2.0
 
 # purge the repo
 # mvn org.owasp:dependency-check-maven:$OLDVERSION:purge
-# mvn org.owasp:dependency-check-maven:$NEWVERSION:purge
+mvn org.owasp:dependency-check-maven:$NEWVERSION:purge
 
 # update the repo
-mvn org.owasp:dependency-check-maven:$OLDVERSION:update-only
+#mvn org.owasp:dependency-check-maven:$OLDVERSION:update-only
 mvn org.owasp:dependency-check-maven:$NEWVERSION:update-only
 
 # run the tests
-mvn -X -B org.owasp:dependency-check-maven:$OLDVERSION:check org.owasp:dependency-check-maven:$OLDVERSION:aggregate \
-  -Dformat=ALL -DskipProvidedScope=true -DautoUpdate=false -DnuspecAnalyzerEnabled=false \
-  -DassemblyAnalyzerEnabled=false -DnodeAuditAnalyzerEnabled=false -DnodeAnalyzerEnabled=false \
-  -DrubygemsAnalyzerEnabled=false -DbundleAuditAnalyzerEnabled=false > $OLDVERSION.log
+#mvn -X -B org.owasp:dependency-check-maven:$OLDVERSION:check org.owasp:dependency-check-maven:$OLDVERSION:aggregate \
+#  -Dformat=ALL -DskipProvidedScope=true -DautoUpdate=false -DnuspecAnalyzerEnabled=false \
+#  -DassemblyAnalyzerEnabled=false -DnodeAuditAnalyzerEnabled=false -DnodeAnalyzerEnabled=false \
+#  -DrubygemsAnalyzerEnabled=false -DbundleAuditAnalyzerEnabled=false > $OLDVERSION.log
   
 mvn -X -B org.owasp:dependency-check-maven:$NEWVERSION:check org.owasp:dependency-check-maven:$NEWVERSION:aggregate \
   -Dformat=ALL -DskipProvidedScope=true -DautoUpdate=false -DnuspecAnalyzerEnabled=false \
