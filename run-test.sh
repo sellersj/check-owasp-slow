09045e0272f3ba7be9e21d8c3d5e4987c2826277
Jim Sellers <jim.sellers@gmail.com>
2022-02-28 11:28:27 -0500

    updating script to test version 7.0.0

--- run-test.sh
+++ run-test.sh
@@ -3,22 +3,22 @@
 # exit if any of the commands return an error
 set -e
 
-OLDVERSION=6.5.2
-NEWVERSION=6.5.3
+OLDVERSION=6.5.3
+NEWVERSION=7.0.0
 
-# purge the repo
-# mvn org.owasp:dependency-check-maven:$OLDVERSION:purge
-# mvn org.owasp:dependency-check-maven:$NEWVERSION:purge
-
-# update the repo
-# mvn org.owasp:dependency-check-maven:$OLDVERSION:update-only
-# mvn org.owasp:dependency-check-maven:$NEWVERSION:update-only
+# purge and reupdate the repo for the old version
+mvn org.owasp:dependency-check-maven:$OLDVERSION:purge
+mvn org.owasp:dependency-check-maven:$OLDVERSION:update-only
 
 # run the tests
 mvn -X -B org.owasp:dependency-check-maven:$OLDVERSION:check org.owasp:dependency-check-maven:$OLDVERSION:aggregate \
   -Dformat=ALL -DskipProvidedScope=true -DautoUpdate=false -DnuspecAnalyzerEnabled=false \
   -DassemblyAnalyzerEnabled=false -DnodeAuditAnalyzerEnabled=false -DnodeAnalyzerEnabled=false \
   -DrubygemsAnalyzerEnabled=false -DbundleAuditAnalyzerEnabled=false > $OLDVERSION.log
+
+# purge and reupdate the repo for the new version
+mvn org.owasp:dependency-check-maven:$NEWVERSION:purge
+mvn org.owasp:dependency-check-maven:$NEWVERSION:update-only
 
 mvn -X -B org.owasp:dependency-check-maven:$NEWVERSION:check org.owasp:dependency-check-maven:$NEWVERSION:aggregate \
   -Dformat=ALL -DskipProvidedScope=true -DautoUpdate=false -DnuspecAnalyzerEnabled=false \
